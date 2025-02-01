a5fe2a392749b041e01c6c82c332001fe53e2081
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-01 12:30:10 +0600

    adaptive layout

--- src/app/(marketing)/components/navbar.tsx
+++ src/app/(marketing)/components/navbar.tsx
@@ -1,4 +1,6 @@
 import Logo from "@/app/shared/components/styles/Logo";
+import { Button } from "@/app/shared/components/styles/ui/Button";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -6,6 +8,13 @@ const Navbar = () => {
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full"></div>
+        <Button className="sm" variant="outline" asChild>
+          <Link href="/sing-in">Войти</Link>
+        </Button>
+        <Button className="sm" variant="outline" asChild>
+          <Link href="/sing-up">Регистрация</Link>
+        </Button>
       </div>
     </div>
   );
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -21,13 +21,13 @@ const MarketingPage = () => {
       <div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
         <div className="mb-4 flex items-center border show-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
-          Task meneger
+          Тайм менеджмент !)
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-natural-800 mb-6"></h1>
-        <h1>Taskify helps team move</h1>
+        <h1 className="text-3xl">Добро пожаловать !</h1>
         <br />
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-zinc-50 px-4 p-2 rounded-md pb-4 w-fit">
-          work forward.
+          TASK MANEGER
         </div>
       </div>
       <div
@@ -36,7 +36,8 @@ const MarketingPage = () => {
           textFont.className
         )}
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, voluptatum?
+        Taskify – это современное и интуитивно понятное приложение для планирования, организации и контроля выполнения
+        задач !
       </div>
       <br />
       <Button>
