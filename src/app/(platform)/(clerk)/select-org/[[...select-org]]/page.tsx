e432d77159b43379b2a3cc38254621523db05d9a
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-01 17:31:58 +0600

    finishhhhhhhhhhhhhhhhhhhhhh organization

---
+++ src/app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,10 @@
+import { OrganizationList } from "@clerk/nextjs";
+import React from "react";
+
+export default function CreateOrganizationPage() {
+  return (
+    <div>
+      <OrganizationList />
+    </div>
+  );
+}
