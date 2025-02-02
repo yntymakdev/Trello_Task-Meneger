9da091d909e18e9d55acf3732412fe85ca56762f
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-02 11:39:40 +0600

    fixed some bug

---
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrganizationIdLayout = ({children} :{children: React.ReactNode) => {
+  return <div>{children}</div>;
+}; 
+
+export default OrganizationIdLayout;
