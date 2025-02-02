eb5bc54dfef284ee4a6b1146a15dd8a9dd84b4a6
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-02 20:43:24 +0600

    costumized server action

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { create } from "../../../../../../action/creat-boart";
 
 const OrganizationId = () => {
-  return <div>OrganPage</div>;
+  return (
+    <form action={create}>
+      <input id="title" name="title" required placeholder="Enter a board title" className="border-black border p-1" />
+    </form>
+  );
 };
 
 export default OrganizationId;
