beff84a1e8cf5c5000a719a0b50be9990d563cbd
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-02 12:35:23 +0600

    sidebar created

--- src/app/(platform)/(dashboard)/components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/components/sidebar.tsx
@@ -1,6 +1,10 @@
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { useLocalStorage } from "usehooks-ts";
 import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/app/shared/components/styles/ui/Button";
+import Link from "next/link";
+import { Plus } from "lucide-react";
 
 interface SideBarProps {
   storageKey?: string;
@@ -11,11 +15,39 @@ const SideBar = ({ storageKey = "t-sidebar-state" }: SideBarProps) => {
 
   const { organization: activeOrganization, isLoaded: isLoadedOrg } = useOrganization();
   const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({ userMemberships: { infinite: true } }); //
-  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(acc: string[], key: string)=> {
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce((acc: string[], key: string) => {
+    if (expanded[key]) {
+      acc.push(key);
+    }
+    return acc;
+  }, []);
 
-    if(expanded)
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+
+    if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+      return (
+        <>
+          <Skeleton />
+        </>
+      );
+    }
   };
-  return <div>sidebar</div>;
+
+  return (
+    <div className="font-medium text-xs flex items-center mb-1">
+      <span className="pl-4">SideBar</span>
+      <Button asChild type="button" size="icon" variant="ghost" className="ml-auto">
+        <Link href="/select-org">
+          <Plus className="h-4 w-4" />
+        </Link>
+      </Button>
+      //{" "}
+    </div>
+  );
 };
 
 export default SideBar;
