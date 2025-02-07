5de6135e16ad5a9ac167fc71aab80603cc39cddc
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-07 23:47:13 +0600

    organizationpage

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -4,24 +4,21 @@ import { db } from '@/lib/db';
 import { create } from "../../../../../../action/create-board/create-board"; // убедитесь, что create — это правильный путь
 
 const OrganizationIdPage = async () => {
-    const boards = await db.board.findMany();
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-
-        const formData = new FormData(event.target as HTMLFormElement);
-        const title = formData.get('title') as string;
-
-        // Обработка создания доски
-        await create({ title });
-
-        // Можно добавить логику для обновления данных, например, перезагрузки
-    };
+    // const boards = await db.board.findMany();
+    //
+    // const handleSubmit = async (event: React.FormEvent) => {
+    //     event.preventDefault();
+    //
+    //     const formData = new FormData(event.target as HTMLFormElement);
+    //     const title = formData.get('title') as string;
+    //
+    //     await create({ title });
+    // };
 
     return (
         <>
             <div className="flex flex-col space-y-4"></div>
-            <form onSubmit={handleSubmit}>
+            <form action={create}>
                 <input
                     id="title"
                     name="title"
@@ -29,14 +26,14 @@ const OrganizationIdPage = async () => {
                     placeholder="Enter a board title"
                     className="border-black border p-1"
                 />
-                <Button>Добавить</Button>
+                {/*<Button>Добавить</Button>*/}
             </form>
 
-            <div className="space-y-2">
-                {boards.map((board) => (
-                    <div key={board.id}>Board name: {board.title}</div>
-                ))}
-            </div>
+            {/*<div className="space-y-2">*/}
+            {/*    {boards.map((board) => (*/}
+            {/*        <div key={board.id}>Board name: {board.title}</div>*/}
+            {/*    ))}*/}
+            {/*</div>*/}
         </>
     );
 };
