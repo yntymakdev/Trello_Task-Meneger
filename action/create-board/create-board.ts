385a2ddc89115105033e7a63833437d96c80169a
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-07 21:22:29 +0600

    setup costumize Createboard

--- action/create-board/create-board.ts
+++ action/create-board/create-board.ts
@@ -1,7 +1,7 @@
 'use server'
 import {db} from "@/lib/db";
 
-async function create(formData: FormData){
+ export      async function create(formData: FormData){
     const title = formData.get("title") as string;
 
     await  db.board.create({
