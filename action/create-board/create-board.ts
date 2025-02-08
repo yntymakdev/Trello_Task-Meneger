6bff53bf3258a4a0fb40dbf96d1110bbd860af8e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-08 14:29:34 +0600

    createBoard page customize

--- action/create-board/create-board.ts
+++ action/create-board/create-board.ts
@@ -1,14 +1,18 @@
-
 'use server'
-import {db} from "@/lib/db";
-const CreateBoard=z.object({
-    title:z.string(),
-})
- export      async function create(formData: FormData){
-    const {title} = CreateBoard.ettitle") as string;
-    await  db.board.create({
-        data: {
-            title
-        }
+import { db } from "@/lib/db";
+import { z } from "zod";
+
+const CreateBoard = z.object({
+    title: z.string(),
+});
+
+export async function create(formData: FormData) {
+    const { title } = CreateBoard.parse({
+     title: formData.get("title");
     })
- }
+    await db.board.create({
+        data: {
+            title,
+        },
+    });
+}
