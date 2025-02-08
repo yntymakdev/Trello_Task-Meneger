9bdaf14547727483713666ae3704051be17ebcb0
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-08 14:54:24 +0600

    board page delete feature

--- src/app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Button} from "@/app/shared/components/styles/ui/Button";
+import {deleteBoard} from "../../../../../../action/create-board/delete-board";
 interface BoardProps {
     title: string;
     id: string
 
 }
 const DeleteBoard = ({title,id}:BoardProps) => {
+
+    const deleteBoardWithId = deleteBoard.bind(null, id);
     return (
-        <form className='flex items-center gap-x-2'>
+        <form action={deleteBoardWithId} className='flex items-center gap-x-2'>
 <p>
     Board title: {title}
 </p>
