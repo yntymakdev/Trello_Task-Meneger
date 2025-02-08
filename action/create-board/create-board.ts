d708f1302c67bfc8055aeaac02a564135f85c40f
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-08 15:11:08 +0600

    costumize create bord page

--- action/create-board/create-board.ts
+++ action/create-board/create-board.ts
@@ -3,12 +3,22 @@ import { db } from "@/lib/db";
 import { z } from "zod";
 import {revalidatePath} from "next/cache";
 
+export type  State = {
+
+    errors?: {
+        title?: string;
+    },
+    message?: string | null;
+}
+
 const CreateBoard = z.object({
-    title: z.string(),
+    title: z.string().min(3,{
+        message: "Minimum length of 3 letter is required",
+    })
 });
 
-export async function create(formData: FormData) {
-    const { title } = CreateBoard.parse({
+export async function create(prevState:State, formData: FormData) {
+    const validatedFields = CreateBoard.safeParse({
      title: formData.get("title"),
 })
 await db.board.create({
